5b9d57eababb419c33719d22f478907ee90a095a
SarayuNarra <sarayunarra26@gmail.com>
2026-02-25 16:38:10 +0530

    UC4

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,15 +1,26 @@
 public class PalindromeChecker {
     public static void main(String[] args){
         String original = "madam";
-        String reversed = "";
 
-        // Reverse the string using loop
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+        // Convert string to character array
+        char[] characters = original.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
+
+        // Two-pointer comparison
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        // Compare original and reversed string
-        if (original.equals(reversed)) {
+        // Display result
+        if (isPalindrome) {
             System.out.println(original + " is a Palindrome");
         } else {
             System.out.println(original + " is NOT a Palindrome");
